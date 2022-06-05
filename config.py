1aab8fd0c8195f970cd4c0fd41771e377f438946
Petr Zakharov <zakharovp11@gmail.com>
2022-06-05 19:20:14 +0300

    change database from sqlite to postgress

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
-# class Configuration:
-#     DEBUG = True
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 class Configuration:
     DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:password@pg:5432/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
